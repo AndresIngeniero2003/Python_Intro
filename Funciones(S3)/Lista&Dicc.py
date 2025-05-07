4aee91aded62786394c55508977b39b06b1122ec
AndresRestrepoDev <167935191+AndresIngeniero2003@users.noreply.github.com>
2025-05-07 13:18:05 -0500

    Update Lista&Dicc.py

--- Funciones(S3)/Lista&Dicc.py
+++ Funciones(S3)/Lista&Dicc.py
@@ -1,17 +1,3 @@
-# Crea una pequeña agenda en donde se guardara el nombre, el celular, estado civil, genero, todo esto se guardara dentro de un 
-# lista para que tengamos una lista de contactos
-
-#     Agregar un nuevo contacto.
-#     Buscar un contacto por su nombre o celular.
-#     Mostrar todos los contactos.
-#     Modificar un contacto en especifico
-#     Eliminar un contacto.
-
-# 📦 Requisitos
-
-#     Usar un diccionario para guardar la informacion y dentro de una lista guarda todos estos contactos
-#     plus: a la hora de eliminar o modificar que me mustre los contactos existentes para asi verificar cual quiero modificar
-
 lista_contactos = []
 
 #Funciones
